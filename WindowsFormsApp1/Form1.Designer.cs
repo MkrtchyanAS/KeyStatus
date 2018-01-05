e62066ba7d38c0e0f981a3a179e4d22a57aca64f
Армен Мкртчян <MkrtchyanAS@outlook.com>
2018-01-06 02:05:25 +0500

    1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Num";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Caps";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Scroll";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 40);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,77 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            Application.Idle += Application_Idle_Num;
+            Application.Idle += Application_Idle_Caps;
+            Application.Idle += Application_Idle_Scroll;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static bool NumLockActive()
+        {
+            return Control.IsKeyLocked(Keys.NumLock);
+        }
+        public static bool CapsLockActive()
+        {
+            return Control.IsKeyLocked(Keys.CapsLock);
+        }
+        public static bool ScrollLockActive()
+        {
+            return Control.IsKeyLocked(Keys.Scroll);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        void Application_Idle_Num(object sender, EventArgs e)
+        {
+            if (NumLockActive())
+            {
+                label1.ForeColor = Color.Green;
+            }
+            else
+            {
+                label1.ForeColor = Color.Gray;
+            }
+        }
+        void Application_Idle_Caps(object sender, EventArgs e)
+        {
+            if (CapsLockActive())
+            {
+                label2.ForeColor = Color.Green;
+            }
+            else
+            {
+                label2.ForeColor = Color.Gray;
+            }
+        }
+        void Application_Idle_Scroll(object sender, EventArgs e)
+        {
+            if (ScrollLockActive())
+            {
+                label3.ForeColor = Color.Green;
+            }
+            else
+            {
+                label3.ForeColor = Color.Gray;
+            }
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            Application.Idle -= Application_Idle_Num;
+            Application.Idle -= Application_Idle_Scroll;
+            Application.Idle -= Application_Idle_Caps;
+            base.OnFormClosed(e);
         }
     }
+
 }
